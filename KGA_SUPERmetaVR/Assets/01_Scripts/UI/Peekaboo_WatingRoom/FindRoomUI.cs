a1e9be95dc1beb0a43f55b70c55398e0d14bffc2
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-10-08 17:23:40 +0900

    prefeb : 대기실UI

--- KGA_SUPERmetaVR/Assets/01_Scripts/UI/Peekaboo_WatingRoom/FindRoomUI.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/UI/Peekaboo_WatingRoom/FindRoomUI.cs
@@ -8,9 +8,21 @@ using Photon.Realtime;
 
 public class FindRoomUI : MonoBehaviourPunCallbacks
 {
+    [Header("방찾기")]
     [SerializeField] TMP_InputField roomNumber;
     [SerializeField] Button FindButton;
     [SerializeField] Button ExitButton;
+
+    [Header("비밀번호 입력")]
+    [SerializeField] GameObject passwordInputUI;
+    [SerializeField] Button checkButton;
+    [SerializeField] Button cancelButton;
+    private TMP_InputField passWord;
+
+    [Header("알림")]
+    [SerializeField] GameObject alramUI;
+    private Button yesButton;
+
     private void Awake()
     {
         FindButton.onClick.AddListener(OnClickFindButton);
