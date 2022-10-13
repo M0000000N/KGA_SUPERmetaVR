8be9861f67c1628699581027aa2521ecd770fadc
ProgrammerPenguin <103081537+ProgrammerPenguin@users.noreply.github.com>
2022-10-13 19:25:33 +0900

    합병 후 오류 수정

--- KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/Manager/GameManager.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/Manager/GameManager.cs
@@ -9,30 +9,32 @@ public class GameManager : SingletonBehaviour<GameManager>
     PlayerData playerData;
     public PlayerData PlayerData { get { return playerData; } set { playerData = value; } }
 
-    void Awake()
-    {
-        Initialize();
-    }
+    //void Awake()
+    //{
+    //    Initialize();
+    //}
 
-    public void Initialize()
-    {
-        playerData = new PlayerData();
-        PlayerData.PlayerPeekabooData = new PlayerPeekabooData();
-        // 테스트 코드
-        GameManager.Instance.PlayerData.PlayerPeekabooData.SelectCharacter = 1;
-        GameManager.Instance.PlayerData.PlayerPeekabooData.Character = new int[5];
-        for (int i = 0; i < GameManager.Instance.PlayerData.PlayerPeekabooData.Character.Length; i++)
-        {
-            GameManager.Instance.PlayerData.PlayerPeekabooData.Character[i] = i;
-        }
-        // 테스트 코드
-    }
+    //public void Initialize()
+    //{
+    //    playerData = new PlayerData();
+    //    PlayerData.PlayerPeekabooData = new PlayerPeekabooData();
+    //    // 테스트 코드
+    //    GameManager.Instance.PlayerData.PlayerPeekabooData.SelectCharacter = 1;
+    //    GameManager.Instance.PlayerData.PlayerPeekabooData.Character = new int[5];
+    //    for (int i = 0; i < GameManager.Instance.PlayerData.PlayerPeekabooData.Character.Length; i++)
+    //    {
+    //        GameManager.Instance.PlayerData.PlayerPeekabooData.Character[i] = i;
+    //    }
+    //    // 테스트 코드
+    //}
 
     public GameObject PlayerPrefeb;
     public Button exitButton;
     [SerializeField]
     private PeekabooCreateMap createMap;
     public PeekabooCreateMap CreateMap { get { return createMap; } }
+
+    public GameObject TestNPC;
 
     public void Start()
     {
