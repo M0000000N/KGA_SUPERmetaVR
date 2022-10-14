e5d2e904f5c865e7ab12ab2751cd1ed2d0d312f5
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-10-14 15:42:39 +0900

    cs : 나가기버튼 -> 확인 -> DKBB로 이동, 방찾기구현 scene : DKBB추가

--- KGA_SUPERmetaVR/Assets/01_Scripts/LobbyManager.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/LobbyManager.cs
@@ -92,8 +92,6 @@ public class LobbyManager : SingletonBehaviour<LobbyManager>
         // PhotonNetwork.NickName = GameManager.Instance.PlayerData.Nickname;
 
         PhotonNetwork.JoinOrCreateRoom(SetRoomName(), _roomOptions, TypedLobby.Default);
-
-        Peekaboo_WaitingRoomUIManager.Instance.PlayRoomUI.gameObject.SetActive(true);
     }
 
     public override void OnJoinedRoom()
@@ -106,6 +104,32 @@ public class LobbyManager : SingletonBehaviour<LobbyManager>
         // 빈 방이 없음, 참여가능 방 없음 팝업
         // [To Do] db에서 텍스트 받아오기
         Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "현재 참여가능한 방이 없습니다.\n잠시 후 다시 시도해주세요.", "확인");
+
+        switch (returnCode) // TODO : 나중에 데이터로 빼야함
+        {
+            case -1:
+                // 서버에 문제가 발생했습니다. 재생산을 시도하고 Exit Game에 문의하십시오.
+                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "서버에 문제가 발생했습니다. 다시 시도해 주세요", "확인");
+                break;
+            case 32765:
+                // 게임이 꽉 찼습니다. 참가가 완료되기 전에 일부 플레이어가 방에 참가한 경우에는 거의 발생하지 않습니다.
+                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "게임이 꽉 찼습니다.", "확인");
+                break;
+            case 32764:
+                // 게임이 종료되어 참가할 수 없습니다. 다른 게임에 참여하세요.
+                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "게임이 종료되어 참가할 수 없습니다. 다른 게임에 참여하세요.", "확인");
+                break;
+            case 32760:
+                // 무작위 매치메이킹은 닫히거나 꽉 차지 않은 방이 있는 경우에만 성공합니다. 몇 초 후에 반복하거나 새 방을 만드십시오.
+                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "방에 들어갈 수 없습니다.", "확인");
+                break;
+            default:
+                // 아무튼 방에 참가하지 못 했다.
+                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "오류가 발생했습니다.", "확인");
+                break;
+        }
+
+        Debug.Log("조인랜덤룸 실패 리턴 코드 : " +returnCode);
     }
 
     public string SetRoomName()
--- KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/WatingRoomUI/FindRoomUI.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/WatingRoomUI/FindRoomUI.cs
@@ -16,75 +16,39 @@ public class FindRoomUI : MonoBehaviourPunCallbacks
     [Header("비밀번호 입력")]
     [SerializeField] GameObject passwordInputUI;
     [SerializeField] Button checkButton;
-    [SerializeField] Button cancelButton;
+    [SerializeField] Button cancleButton;
     private TMP_InputField passWord;
 
     private void Awake()
     {
         findButton.onClick.AddListener(OnClickFindButton);
         exitButton.onClick.AddListener(OnClickExitButton);
+        checkButton.onClick.AddListener(OnClickCheckButton);
+        cancleButton.onClick.AddListener(OnClickCancleButton);
     }
 
     private void Start()
     {
-        passwordInputUI.SetActive(false);
+        SetPasswordInputUI(false);
     }
 
     public void OnClickFindButton()
     {
-        if(passwordInputUI.activeSelf)
+        if(true) // 실존하는 룸일 때
         {
-            if (PhotonNetwork.JoinRoom(roomNumber.text + "_" + passWord.text))
+            if (true) // publicRoom
             {
-                UnityEngine.Debug.Log("방 입장.");
-                Peekaboo_WaitingRoomUIManager.Instance.PlayRoomUI.gameObject.SetActive(true);
+                PhotonNetwork.JoinRoom(roomNumber.text);
                 OnClickExitButton();
             }
-            else
+            else // privateRoom
             {
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "방에 입장할 수 없습니다.", "확인"); // TODO : 나중에 데이터로 빼야함
+                SetPasswordInputUI(true);
             }
-            SetPopupButtonUI(false);
         }
         else
         {
-            if(PhotonNetwork.JoinRoom(roomNumber.text))
-            {
-                UnityEngine.Debug.Log("방 입장.");
-                Peekaboo_WaitingRoomUIManager.Instance.PlayRoomUI.gameObject.SetActive(true);
-                OnClickExitButton();
-            }
-            else
-            {
-                SetPopupButtonUI(true);
-            }
-        }
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        switch (returnCode) // TODO : 나중에 데이터로 빼야함
-        {
-            case -1:
-                // 서버에 문제가 발생했습니다. 재생산을 시도하고 Exit Game에 문의하십시오.
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "서버에 문제가 발생했습니다. 다시 시도해 주세요", "확인");
-                break;
-            case 32765:
-                // 게임이 꽉 찼습니다. 참가가 완료되기 전에 일부 플레이어가 방에 참가한 경우에는 거의 발생하지 않습니다.
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "게임이 꽉 찼습니다.", "확인");
-                break;
-            case 32764:
-                // 게임이 종료되어 참가할 수 없습니다. 다른 게임에 참여하세요.
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "게임이 종료되어 참가할 수 없습니다. 다른 게임에 참여하세요.", "확인");
-                break;
-            case 32760:
-                // 무작위 매치메이킹은 닫히거나 꽉 차지 않은 방이 있는 경우에만 성공합니다. 몇 초 후에 반복하거나 새 방을 만드십시오.
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "방에 들어갈 수 없습니다.", "확인");
-                break;
-            default:
-                // 아무튼 방에 참가하지 못 했다.
-                Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "오류가 발생했습니다.", "확인");
-                break;
+            Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "존재하지 않는 방 번호입니다.\n다시 한번 확인해주세요.", "확인"); // TODO : 나중에 데이터로 빼야함
         }
     }
 
@@ -93,8 +57,26 @@ public class FindRoomUI : MonoBehaviourPunCallbacks
         gameObject.SetActive(false);
     }
 
-    public void SetPopupButtonUI(bool _isActive)
+    public void OnClickCheckButton()
+    {
+        if(true) // passWord.text == 커스텀프로퍼티 비번
+        {
+            PhotonNetwork.JoinRoom(roomNumber.text);
+        }
+        else
+        {
+            Peekaboo_WaitingRoomUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "비밀번호가 일치하지 않습니다.\n다시 한번 확인해주세요.", "확인"); // TODO : 나중에 데이터로 빼야함
+        }
+    }
+
+    public void OnClickCancleButton()
+    {
+        SetPasswordInputUI(false);
+    }
+
+    private void SetPasswordInputUI(bool _isActive)
     {
         passwordInputUI.SetActive(_isActive);
     }
+
 }
