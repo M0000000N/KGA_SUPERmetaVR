63082b5944f6cea1c9a91f77c78a8855de5af1fa
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-10-18 21:46:42 +0900

    방 생성 비밀번호 예외처리

--- KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/MainUI/PKB_CreateRoomUI.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/MainUI/PKB_CreateRoomUI.cs
@@ -41,32 +41,32 @@ public class PKB_CreateRoomUI : MonoBehaviourPunCallbacks
             // privateRoom
             grayText.SetActive(false);
             passwordInput.interactable = true;
+
+            if (passwordInput.text.Length == 0)
+            {
+                createButton.interactable = false;
+            }
+            else
+            {
+                createButton.interactable = true;
+            }
         }
         else
         {
             // publicRoom
             grayText.SetActive(true);
+            passwordInput.text = "";
             passwordInput.interactable = false;
+            createButton.interactable = true;
         }
-    }
-
-    public void SetPassword()
-    {
-        if(passwordInput.text.Length > 8)
-        {
-            passwordInput.interactable = false;
-        }
-        password = passwordInput.text;
     }
 
     public void OnClickCreateButton()
     {
-        gameObject.SetActive(false);
-
         if (privateRoom.isOn)
         {
             // privateRoom
-            if(false) //  TODO : 유니티 다이얼 키패드 있는지 봐야함
+            if (false) //  TODO : 비밀번호 규칙에 어긋날 때(유니티 다이얼 키패드 있는지 봐야함)
             {
                 // TODO : 나중에 데이터로 빼야함
                 PKB_MainUIManager.Instance.NoticePopupUI.SetNoticePopup("알림", "비밀번호는 1자리 최대 8자리\n숫자만 사용 가능합니다.", "확인");
@@ -80,6 +80,16 @@ public class PKB_CreateRoomUI : MonoBehaviourPunCallbacks
             // publicRoom
             LobbyManager.Instance.CreateRoom(null);
         }
+        gameObject.SetActive(false);
+    }
+
+    public void SetPassword()
+    {
+        if (passwordInput.text.Length > 8)
+        {
+            passwordInput.interactable = false;
+        }
+        password = passwordInput.text;
     }
 
     public void OnClickExitButton()
