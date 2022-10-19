c1fa5a9fb4958fc85b69b5903cd6d351efd2faf4
choiseonu <ydcsw@naver.com>
2022-10-19 19:05:45 +0900

    Revert "Revert "Revert "Revert "PC 프리팹 재수정""""
    This reverts commit e287c869f8e78662fc3939ad40c02f1c59e2129f.

--- KGA_SUPERmetaVR/Assets/01_Scripts/PC/PlayerMove/PlayMove_Photon.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/PC/PlayerMove/PlayMove_Photon.cs
@@ -159,6 +159,17 @@ public class PlayMove_Photon : MonoBehaviourPun, IPunObservable
         }
     }
 
+    public void CullingPCModeling()
+    {
+        if(photonView.IsMine)
+        {
+            if(this.tag == "PCModel")
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
 }
 
 
