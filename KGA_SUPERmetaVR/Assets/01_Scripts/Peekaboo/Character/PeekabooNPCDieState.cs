b7c5484e2ce6cdf092d6a536f1095e64cf3be5ee
2sseung1 <bigseung12@gmail.com>
2022-10-26 19:00:01 +0900

    NPC 리스폰 Material 교체 안되는 버그 임시로 해결

--- KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/Character/PeekabooNPCDieState.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Peekaboo/Character/PeekabooNPCDieState.cs
@@ -9,15 +9,17 @@ public class PeekabooNPCDieState : PeekabooCharacterState
     [SerializeField]
     private Renderer myRenderer;
     [SerializeField]
+    public Material opaqueMaterial;
+    [SerializeField]
     private Material fadeMaterial;
     [SerializeField]
     private NavMeshAgent playerNavMeshAgent;
-
-    public Material myMaterial;
-
+    [SerializeField]
+    private GameObject myBody;
+   
     protected override void Initialize()
     {
-        myMaterial = myRenderer.material;
+
     }
 
     public override void OnEnter()
@@ -43,7 +45,6 @@ public class PeekabooNPCDieState : PeekabooCharacterState
 
     private IEnumerator DieCoroutine(float _time)
     {
-        // myMaterial = myRenderer.material;
         myRenderer.material = fadeMaterial;
         Color myColor = myRenderer.material.color;
         float decreaseValue = 1 / _time;
@@ -55,14 +56,15 @@ public class PeekabooNPCDieState : PeekabooCharacterState
             yield return null;
         }
 
-        // photonView.RPC("StartRespawn", RpcTarget.All);
+        StartCoroutine(Peekaboo(3f));
         StartCoroutine(RespawnCoroutine(3f));
     }
 
-    [PunRPC]
-    private void StartRespawn()
+    private IEnumerator Peekaboo(float _time)
     {
-        StartCoroutine(RespawnCoroutine(3f));
+        yield return new WaitForSeconds(_time);
+
+        myRenderer.material = opaqueMaterial;
     }
 
     private IEnumerator RespawnCoroutine(float _time)
@@ -70,15 +72,15 @@ public class PeekabooNPCDieState : PeekabooCharacterState
         yield return new WaitForSeconds(_time);
 
         playerNavMeshAgent.enabled = false;
-        transform.position = PeekabooGameManager.Instance.PeekabooSpawner.RespawnNPC(transform.position);  
-        transform.position += Vector3.up * 15f;
-        myRenderer.material = myMaterial;
-        Color myColor = myMaterial.color;
-        myColor.a = 255f;
+        myBody.transform.position = PeekabooGameManager.Instance.PeekabooSpawner.RespawnNPC(transform.position);  
+        myBody.transform.position += Vector3.up * 15f;
+        //myRenderer.material = opaqueMaterial;
+        Color myColor = myRenderer.material.color;
+        myColor.a = 1f;
         myRenderer.material.color = myColor;
-        while (transform.position.y > 1.2f)
+        while (myBody.transform.position.y > 1.2f)
         {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 1, transform.position.z), 0.01f);
+            myBody.transform.position = Vector3.Lerp(myBody.transform.position, new Vector3(myBody.transform.position.x, 1, myBody.transform.position.z), 0.01f);
             yield return new WaitForSeconds(0.01f);
         }
 
