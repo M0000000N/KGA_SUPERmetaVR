4257f7df5d5ce8ccddf35c60a030dba22cf1e0c9
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-10-27 14:07:47 +0900

    GameManager : 플레이구역 나갔을 때 경고 UI 출력

---
+++ KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/FeeFawFum/FFF_GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFF_GameManager : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        // 경고 UI 출력
+    }
+}
