90195ae4e021d1313aadee9fd6942240875d2993
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-01 15:18:18 +0900

    종이학 잡은 후 5초뒤 사라짐, 세잎 클로버 2초뒤 사라짐

--- KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/PaperSwan/PS_GameManager.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/PaperSwan/PS_GameManager.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PS_GameManager : OnlyOneSceneSingleton<PS_GameManager>
 {
     public bool IsCoolTime;
-    // Start is called before the first frame update
     void Start()
     {
         IsCoolTime = false;
     }
-    public void OnTriggerExit(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        other.gameObject.SetActive(false);
+        if(other.tag == "PaperSwan")
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
