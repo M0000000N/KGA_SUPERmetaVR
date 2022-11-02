e720b2a78f0f3ec4757b4cac3b981b783b871bbe
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-02 09:45:25 +0900

    플레이어 그랩 시스템 변경

---
+++ KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/FeeFawFum/FFF_Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.EventSystems;
+
+public class FFF_Player : MonoBehaviour
+{
+    [SerializeField] GameObject leftHand;
+    private XRRayInteractor leftRayInteractor;
+    private XRController leftXRController;
+    private LineRenderer leftLineRenderer;
+    private RaycastResult leftRaycastResult;
+
+    [SerializeField] GameObject rightHand;
+    private XRRayInteractor rightRayInteractor;
+    private XRController rightXRController;
+    private LineRenderer rightLineRenderer;
+    private RaycastResult rightRaycastResult;
+
+    private GameObject targetObject;
+
+    void Start()
+    {
+        leftRayInteractor = leftHand.GetComponent<XRRayInteractor>();
+        leftXRController = leftHand.GetComponent<XRController>();
+        leftLineRenderer = leftHand.GetComponent<LineRenderer>();
+
+        rightRayInteractor = rightHand.GetComponent<XRRayInteractor>();
+        rightXRController = rightHand.GetComponent<XRController>();
+        rightLineRenderer = rightHand.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (FFF_GameManager.Instance.isReady)
+        {
+            SetRay(0.2f, false);
+            RayCastHit();
+        }
+        else
+        {
+            SetRay(20, true);
+        }
+    }
+
+    private void SetRay(float _maxRaycastDistance, bool _useWorldSpace)
+    {
+        leftRayInteractor.maxRaycastDistance = _maxRaycastDistance;
+        rightRayInteractor.maxRaycastDistance = _maxRaycastDistance;
+        leftLineRenderer.useWorldSpace = _useWorldSpace;
+        rightLineRenderer.useWorldSpace = _useWorldSpace;
+    }
+
+    public void RayCastHit()
+    {
+        if (rightRayInteractor.TryGetCurrentUIRaycastResult(out rightRaycastResult))
+        {
+            rightRaycastResult.gameObject.GetComponent<Button>().interactable = false;
+        }
+        else if (leftRayInteractor.TryGetCurrentUIRaycastResult(out leftRaycastResult))
+        {
+            leftRaycastResult.gameObject.GetComponent<Button>().interactable = false;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
