f6bc8722bb7f63e8eaefec78f1bb33c1ff9d99c0
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-04 14:17:41 +0900

    손 쥐기 애니메이션 적용

---
+++ KGA_SUPERmetaVR/Assets/05_Packages/VR_Hand/3.Scripts/Share/Hand/XRHandController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public enum HandType
+{
+    Left,
+    Right
+}
+
+public class XRHandController : MonoBehaviour
+{
+    public HandType handType;
+    public float thumbMoveSpeed = 0.1f;
+
+    private Animator animator;
+    private InputDevice inputDevice;
+
+    private float indexValue;
+    private float thumbValue;
+    private float threeFingersValue;
+
+    private bool inputDeviceValid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputDeviceValid = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inputDeviceValid)
+        {
+            initInputDevice();
+        }
+        AnimateHand();
+    }
+
+    void initInputDevice()
+    {
+        InputDeviceCharacteristics controllerCharacteristic = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Controller;
+
+        if (handType == HandType.Left)
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Left;
+        }
+        else
+        {
+            controllerCharacteristic = controllerCharacteristic | InputDeviceCharacteristics.Right;
+        }
+
+        List<InputDevice> inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristic, inputDevices);
+        //InputDevices.GetDevices(inputDevices);
+
+
+        if (inputDevices != null && inputDevices.Count > 0)
+        {
+            inputDevice = inputDevices[0];
+            inputDeviceValid = true;
+        }
+    }
+
+    void AnimateHand()
+    {
+        inputDevice.TryGetFeatureValue(CommonUsages.trigger, out indexValue);
+        inputDevice.TryGetFeatureValue(CommonUsages.grip, out threeFingersValue);
+
+        inputDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out bool primaryTouched);
+        inputDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out bool secondaryTouched);
+
+        if (primaryTouched || secondaryTouched)
+        {
+            thumbValue += thumbMoveSpeed;
+        }
+        else
+        {
+            thumbValue -= thumbMoveSpeed;
+        }
+
+        thumbValue = Mathf.Clamp(thumbValue, 0, 1);
+
+        animator.SetFloat("Index", indexValue);
+        animator.SetFloat("ThreeFingers", threeFingersValue);
+        animator.SetFloat("Thumb", thumbValue);
+    }
+}
