0db2be7d49981fb620c72f8f097cbbd27c0ee094
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-10 17:01:06 +0900

    그랩 시스템 변경(버그해결)

--- KGA_SUPERmetaVR/Assets/04_Scenes/Inventory/Scripts/ItemSelect.cs
+++ KGA_SUPERmetaVR/Assets/04_Scenes/Inventory/Scripts/ItemSelect.cs
@@ -1,152 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.EventSystems;
-using UnityEngine.Animations.Rigging;
+using UnityEngine.InputSystem;
 
 public class ItemSelect : MonoBehaviour
 {
     [SerializeField] GameObject leftHand;
-    [SerializeField] private Material defaultMaterial;
-    [SerializeField] private Material fadeMaterial;
-    
     private XRRayInteractor leftRayInteractor;
-
     private GameObject targetObject;
-    
-    private float fadeoutTime = 2f;
+    private GameObject grabObject;
 
-    private bool isGrabItem;
-    private bool isStartdestroy;
-    private bool isStartFadedout;
+    [SerializeField] private InputActionProperty isGrap;
+
+    private bool isDestroyCloverRun = false;
+    private bool isGrabRun = false;
 
     void Start()
     {
         leftRayInteractor = leftHand.GetComponent<XRRayInteractor>();
-
-        isGrabItem = false;
-        isStartdestroy = false;
-        isStartFadedout = false;
     }
 
     private void Update()
     {
-        if (isStartdestroy)
+        if (isGrap.action.IsPressed() && isGrabRun == false)
         {
-            StartCoroutine("DestroyObject");
+            Grab();
         }
-        if (isStartFadedout)
+        else if (isGrap.action.IsPressed() == false && isGrabRun)
         {
-            StartCoroutine("FadeoutRespawnClover", targetObject);
+            GrabOut();
         }
     }
 
-    /// <summary>
-    /// 쥐고있지 않을 때 타겟오브젝트 실시간 설정
-    /// </summary>
     public void HoverGet3DRayCastHit()
     {
-        if (leftRayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit _leftRayHit) && isGrabItem == false)
+        //raycastall vs raycast 
+        if (leftRayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit _leftRayHit))
         {
-            string targetTag = _leftRayHit.transform.gameObject.tag;
+            if (isGrabRun == false)
+            {
+                string targetTag = _leftRayHit.transform.gameObject.tag;
 
-            if (targetTag == "ThreeLeafClover" || targetTag == "FourLeafClover")
-            {
-                targetObject = _leftRayHit.transform.gameObject;
+                if (targetTag == "ThreeLeafClover" || targetTag == "FourLeafClover")
+                {
+                    targetObject = _leftRayHit.transform.gameObject;
+                }
+                else
+                {
+                    targetObject = null;
+                }
             }
-            else
-            {
-                targetObject = null;
-            }
+        }
+        else
+        {
+            targetObject = null;
         }
     }
 
-    /// <summary>
-    /// 쥐었을 때 태그별 함수 실행
-    /// </summary>
-    public void GrabHand()
+    private void Grab()
     {
-        isGrabItem = true;
-        string targetTag = "";
+        isGrabRun = true;
+        string targetTag = string.Empty;
         if (targetObject != null)
         {
-            targetTag = targetObject.tag;
+            grabObject = targetObject;
+            targetTag = grabObject.tag;
         }
-        if (targetTag == "ThreeLeafClover") // 세잎클로버면
+        if (targetTag.Equals("ThreeLeafClover")) // 세잎클로버면
         {
-            if (isStartdestroy == false)
+            if (isDestroyCloverRun == false)
             {
-                isStartdestroy = true;
+                StartCoroutine(DestroyObject());
             }
         }
-        if (targetTag == "FourLeafClover") // 네잎클로버면
+        else if (targetTag.Equals("FourLeafClover")) // 네잎클로버면
         {
             //  TODO : 멋진 파티클효과
         }
-        // GetComponent<XRRayInteractor>().enabled = false;
     }
 
-    /// <summary>
-    /// 떼었을 때 페이드아웃 실행
-    /// </summary>
-    public void GrabOutHand()
+    private void GrabOut()
     {
-        isGrabItem = false;
-        if (isStartFadedout == false)
-        {
-            // 이걸 하는 이유는 같은 클로버에서 페이드아웃 코루틴을 두번 실행하는 걸 방지하기 위함이다
-                    // 근데 각기 다른 클로버는 페이드아웃 코루틴을 동시에 진행할 수 있음
-                // 그럼 페이드아웃은 각각의 클로버가 실행하는게 맞는듯
-                // 그럼 같은 클로버의 페이드아웃 코루틴을 두번 실항할 일이 없다
-            StopCoroutine("DestroyObject"); // 이거떄문에 이전에 잡고있던 클로버를 2초지나기 전에 떼면 페이드아웃이 안됨
+        isGrabRun = false;
+        if (grabObject == null) return;
+        CloverInfo targetCloverInfo = grabObject.GetComponent<CloverInfo>();
+        if (targetCloverInfo == null) return;
 
-            isStartFadedout = true;
+        if (targetCloverInfo.IsStartFadedout == false)
+        {
+            leftRayInteractor.enableInteractions = false;
+            StartCoroutine(Activeinteractor());
+
+            StopCoroutine(DestroyObject());
+            targetCloverInfo.IsStartFadedout = true; 
+            isDestroyCloverRun = false;
+            targetObject = null;
+            grabObject = null;
         }
-        // GetComponent<XRRayInteractor>().enabled = true;
     }
 
     private IEnumerator DestroyObject()
     {
-        isStartdestroy = false;
+        if (grabObject == null) yield break;
+
+        isDestroyCloverRun = true;
+        CloverInfo targetCloverInfo = grabObject.GetComponent<CloverInfo>();
         yield return new WaitForSeconds(2f);
-        if (isStartFadedout == false)
+        if (targetCloverInfo.IsStartFadedout == false)
         {
-            isStartFadedout = true;
+            StartCoroutine(Activeinteractor());
+
+            targetCloverInfo.IsStartFadedout = true;
+            targetObject = null;
+            grabObject = null;
         }
-        Debug.Log(targetObject.name + "없애라");
+        isDestroyCloverRun = false;
     }
 
-    private IEnumerator FadeoutRespawnClover(GameObject _targetClover)
+    IEnumerator Activeinteractor()
     {
-        string tag = _targetClover.tag;
-        _targetClover.GetComponent<XRGrabInteractable>().enabled = false;
-        _targetClover.tag = "Untagged";
-        Debug.Log(_targetClover.name + "없앤다");
-        isStartFadedout = false;
-
-        MeshRenderer myRenderer = _targetClover.GetComponentInChildren<MeshRenderer>();
-        myRenderer.material = fadeMaterial;
-        Color myColor = myRenderer.material.color;
-        // float decreaseValue = 1 / fadeoutTime;
-
-        while (0 < myRenderer.material.color.a)
-        {
-            myColor.a -= 0.1f / fadeoutTime;
-            myRenderer.material.color = myColor;
-            yield return new WaitForSeconds(0.1f);
-        }
-        Debug.Log(_targetClover.name + "없어졌다");
-
-        _targetClover.SetActive(false);
-        myRenderer.material = defaultMaterial;
-
-        yield return new WaitForSeconds(0.1f);
-        CloverSpawnManager.Instance.ReSpawnClover(_targetClover.transform, _targetClover.GetComponent<CloverInfo>().Area);
-        Debug.Log(_targetClover.name + "리스폰한다");
-        _targetClover.tag = tag;
-        _targetClover.GetComponent<XRGrabInteractable>().enabled = true;
+        yield return new WaitForSeconds(2f);
+        leftRayInteractor.enableInteractions = true;
 
     }
 
