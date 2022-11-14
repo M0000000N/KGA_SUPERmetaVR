451c06cf30adaa2699091eee1516df60c402e44f
BBNO <bbno1105@gmail.com>
2022-11-14 14:49:05 +0900

    아이템 랜덤 획득 코드 작성

--- KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/RewardManager.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/DKBB/RewardManager.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class RewardManager : SingletonBehaviour<RewardManager>
 {
-    //public int GetItem()
-    //{
+    public int ChooseItem()
+    {
+        int rewardItemProbability = Random.Range(0, 100000);
+        for (int i = 0; i < StaticData.RewardSheetData.Length; i++)
+        {
+            int probability = StaticData.GetRewardData(i).Probability;
+            if (rewardItemProbability <= probability)
+            {
+                return StaticData.GetRewardData(i).Itemid;
+            }
+            rewardItemProbability -= probability;
+        }
+        return 0;
+    }
 
-    //}
+    public bool GetItme()
+    {
+        int rewardItemID = ChooseItem();
+        if(rewardItemID > 0)
+        {
+            // æ∆¿Ã≈€ »πµÊ
+            return true;
+        }
+        return false;
+    }
 }
