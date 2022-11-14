c6dceab43fec2bc4d3cec684c683854db63a8533
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-14 19:19:43 +0900

    파티클 에셋 적용

--- KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FlyDragon/FD_Dragon.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FlyDragon/FD_Dragon.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class FD_Dragon : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    // Start is called before the first frame update
+    private ParticleSystem[] particle;
+    private Rigidbody rigidbody;
+
     void Start()
     {
+        particle = GetComponentsInChildren<ParticleSystem>();
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (transform.position.y >= 40)
         {
             rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    public void ParticleOn()
+    {
+        for (int i = 0; i < particle.Length; i++)
+        {
+            particle[i].Play();
         }
     }
 }
