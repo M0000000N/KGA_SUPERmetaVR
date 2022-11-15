58a0c2593b4fbab70b1e1572db7a141a72f25d7a
2sseung1 <bigseung12@gmail.com>
2022-11-15 11:45:27 +0900

    테스트 환경 구축

---
+++ KGA_SUPERmetaVR/Assets/Seung/TestManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TestManager : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Ver.1_Lobby");
+    }
+}
