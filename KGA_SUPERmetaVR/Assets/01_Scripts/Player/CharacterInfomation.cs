b3e441fde8d881afb2f26e66ff1c0082630229a2
BBNO <bbno1105@gmail.com>
2022-11-16 14:14:33 +0900

    친구 요청 버튼 연결

--- KGA_SUPERmetaVR/Assets/01_Scripts/Player/CharacterInfomation.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Player/CharacterInfomation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class CharacterInfomation : MonoBehaviourPunCallbacks
 {
@@ -11,6 +12,8 @@ public class CharacterInfomation : MonoBehaviourPunCallbacks
 
     [SerializeField] private string nickName;
     public string NickName { get { return nickName; } set { nickName = value; } }
+
+    [SerializeField] private Button friendButton;
 
     PhotonView photonView;
 
@@ -25,12 +28,16 @@ public class CharacterInfomation : MonoBehaviourPunCallbacks
 
             photonView.RPC("setInfo", RpcTarget.All, id, name);
         }
+
+        friendButton.onClick.AddListener(() => { FriendManager.Instance.AddFriend(UID); } );
     }
 
+    // 이거 해봐야함...
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         if (photonView.IsMine)
         {
+            UnityEngine.Debug.Log("OnPlayerEnteredRoom은 : " + newPlayer);
             int id = GameManager.Instance.PlayerData.UID;
             string name = GameManager.Instance.PlayerData.Nickname;
 
@@ -44,5 +51,4 @@ public class CharacterInfomation : MonoBehaviourPunCallbacks
         UID = _id;
         NickName = _name;
     }
-  
 }
