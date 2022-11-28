580e6d69014931f642d7c3bbe728a514e9ae4fd7
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-11-28 19:56:12 +0900

    피포팜 성공실패처리 및 UI

--- KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FeeFawFum/FFF_ButtonList.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FeeFawFum/FFF_ButtonList.cs
@@ -1,27 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class FFF_ButtonList : MonoBehaviour
 {
-    [SerializeField] Button[] button;
-    private int clickCount;
-    public bool IsDone;
+    [SerializeField] FFF_Button[] button;
+    [SerializeField] Sprite[] imagePool;
+    public Sprite[] ImagePool { get { return imagePool; } set { imagePool = value; } }
+    private List<Vector3> spawnPositionList = new List<Vector3>();
+    private int round;
 
     private void Start()
     {
-        clickCount = 0;
-        IsDone = false;
+        for (int i = 1; i < button.Length; i++)
+        {
+            button[i].gameObject.SetActive(false);
+        }
+        round = 0;
     }
 
-    public void OnClickButton()
+    private void Update()
     {
-        clickCount++;
-        if (clickCount == button.Length)
+        if (FFF_GameManager.Instance.flow == 2)
         {
-            IsDone = true;
+            int doneCount = FFF_GameManager.Instance.DoneCount;
+            if (doneCount > 0 && doneCount % 2 == 0)
+            {
+                SetNextButtonList(round, false);
+                round++;
+                SetNextButtonList(round, true);
+            }
         }
     }
+
+    public void SetNextButtonList(int _round, bool _isActive)
+    {
+        button[_round].gameObject.SetActive(_isActive);
+        button[_round + 1].gameObject.SetActive(_isActive);
+    }
+
+    //public void Initialize()
+    //{
+    //    for (int i = 0; i < button.Length; i++)
+    //    {
+    //        SpawnObject(button[i].transform);
+    //    }
+    //}
+    //public void SpawnObject(Transform _target)
+    //{
+    //    Vector3 spawnPosition;
+    //    do
+    //    {
+    //        float randomX = Random.Range(-1f, 0.5f);
+    //        float randomY = Random.Range(-0.2f, 0.7f);
+
+    //        spawnPosition = new Vector3(randomX, randomY, 0);
+    //    }
+    //    while (CheckSpawnArea(spawnPosition) == false);
+
+    //    spawnPositionList.Add(spawnPosition);
+
+    //    _target.position = spawnPosition;
+    //}
+
+    //public bool CheckSpawnArea(Vector3 _position)
+    //{
+    //    for (int i = 0; i < spawnPositionList.Count; i++)
+    //    {
+
+    //        if (spawnPositionList[i] == _position)
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //    return true;
+    //}
 }
--- KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FeeFawFum/FFF_GameManager.cs
+++ KGA_SUPERmetaVR/Assets/01_Scripts/Lobby/FeeFawFum/FFF_GameManager.cs
@@ -6,17 +6,61 @@ public class FFF_GameManager : OnlyOneSceneSingleton<FFF_GameManager>
 {
     public int flow; // 0 : 레디 전, 1: 퍼즐1(한손 잡기), 2 : 퍼즐2(양손잡기), 3 : 퍼즐3(gui에 손 맞추기)
 
-    public void initioalize()
+    public int Score;
+    public int DoneCount;
+    [SerializeField] Animator[] fffAnimationController;
+    [SerializeField] FFF_ButtonList FFF_ButtonList;
+
+    private void Start()
+    {
+        Initioalize();
+    }
+
+    public void Initioalize()
     {
         flow = 0;
+        Score = 0;
+        DoneCount = 0;
     }
-    private void OnTriggerExit(Collider other)
+
+    public void StartDanceMode()
     {
-        // 경고 UI 출력
-        GuideUI.Instance.ActiveGuideUI("들어가라이놈아", 0);
+        FFF_GameManager.Instance.flow = 2;
+        SoundManager.Instance.PlayBGM("fee_faw_fum_bgm.mp3");
+        SetBoolFFFNPCAnimation("DanceStart", true);
+        FFF_ButtonList.SetNextButtonList(0, true);
     }
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        // 경고 UI사라짐
+        if (FFF_GameManager.Instance.flow == 2)
+        {
+            if (DoneCount >= 15)
+            {
+                if (Score >= 15)
+                {
+                    SetBoolFFFNPCAnimation("DanceStart", false);
+                    SetTriggerFFFNPCAnimation("MissionClear");
+                    Initioalize();
+                    SoundManager.Instance.PlayBGM("ROBEE_bgm.mp3");
+                }
+                //if() 실패했을 때
+            }
+        }
+    }
+
+    private void SetBoolFFFNPCAnimation(string _name, bool _value)
+    {
+        for (int i = 0; i < fffAnimationController.Length; i++)
+        {
+            fffAnimationController[i].SetBool(_name, _value);
+        }
+    }
+
+    private void SetTriggerFFFNPCAnimation(string _name)
+    {
+        for (int i = 0; i < fffAnimationController.Length; i++)
+        {
+            fffAnimationController[i].SetTrigger(_name);
+        }
     }
 }
